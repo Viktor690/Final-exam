dfc13d231e4943b0d44500b5cde675116413a537
Viktor <burjatvic@gmail.com>
2024-03-20 15:31:13 +0800

    Added 4 step in Program

--- Program.cs
+++ Program.cs
@@ -3,3 +3,11 @@
 string[] res = new string[array.Length];
 
 int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{   
+    if (array[i].Length < 4)
+    {
+        res[count++] = array[i];
+    }
+}
